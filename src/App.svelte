e52c096a4a6e6fa87cfb32df5671bb32c964da9d
notfoundnone <nekit.dema@mail.ru>
2025-02-22 23:34:25 +0300

    Added files

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+  import Navbar from './components/Navbar.svelte';
+  import VideoPlayer from './components/VideoPlayer.svelte';
+  import MainPage from './components/MainPage.svelte';
+  let showVideo = $state(false);        
+  let currentVideoId = $state(null);    
+
+  function handleShowVideo(event) {
+    currentVideoId = event.detail.videoId;
+    showVideo = true;
+  }
+
+  function handleHideVideo() {
+    showVideo = false;
+    currentVideoId = null;
+  }
+</script>
+
+<main class="container">
+  <Navbar/>
+  {#if showVideo}
+  <VideoPlayer videoId={currentVideoId} on:close={handleHideVideo} />
+{:else}
+  <MainPage on:showVideo={handleShowVideo}/>
+{/if} 
+</main>
+
+<style>
+  .container {
+    display: flex;
+    flex-direction: column;
+    height: 100vh; 
+   }
+</style>
