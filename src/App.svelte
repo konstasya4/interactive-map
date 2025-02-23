0d5b11146a425b0484e40460beb6b35962f9a8ab
notfoundnone <nekit.dema@mail.ru>
2025-02-23 15:19:15 +0300

    Change main commponent

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,55 @@
 <script>
-  import Navbar from './components/Navbar.svelte';
-  import VideoPlayer from './components/VideoPlayer.svelte';
-  import MainPage from './components/MainPage.svelte';
-  let showVideo = $state(false);        
-  let currentVideoId = $state(null);    
+  import Navbar from "./components/Navbar.svelte";
+  import VideoPlayer from "./components/VideoPlayer.svelte";
+  import BottomPanel from "./components/BottomPanel.svelte";
+  import Map from "./components/Map.svelte";
+
+  let showVideo = $state(false);
+  let currentVideoId = $state(null);
+  let isBottomPanelVisible = $state(false);
 
   function handleShowVideo(event) {
     currentVideoId = event.detail.videoId;
     showVideo = true;
+    isBottomPanelVisible = false; 
   }
 
   function handleHideVideo() {
     showVideo = false;
     currentVideoId = null;
+    isBottomPanelVisible = true; 
   }
 </script>
 
 <main class="container">
-  <Navbar/>
+  <Navbar />
+
   {#if showVideo}
-  <VideoPlayer videoId={currentVideoId} on:close={handleHideVideo} />
-{:else}
-  <MainPage on:showVideo={handleShowVideo}/>
-{/if} 
+    <VideoPlayer videoId={currentVideoId} on:close={handleHideVideo} />
+  {:else}
+    <Map
+      on:bottomPanelVisible={(e) =>
+        (isBottomPanelVisible = e.detail.isBottomPanelVisible)}
+    />
+    {#if isBottomPanelVisible}
+      <BottomPanel
+        on:showVideo={handleShowVideo}
+        on:close={() => (isBottomPanelVisible = false)}
+      />
+    {/if}
+  {/if}
 </main>
 
 <style>
+  body,
+  html {
+    height: 100vh;
+    margin: 0;
+    padding: 0;
+  }
   .container {
     display: flex;
     flex-direction: column;
-    height: 100vh; 
-   }
+    height: 100vh;
+  }
 </style>
